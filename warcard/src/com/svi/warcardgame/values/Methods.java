fabeb58f715c517c3d6cc637a2abb58d0b214124
glucila <49044805+glucila@users.noreply.github.com>
2019-04-04 07:23:44 +0800

    Delete Methods.java

--- warcard/src/com/svi/warcardgame/values/Methods.java
+++
@@ -1,7 +0,0 @@
-package com.svi.warcardgame.values;
-
-public interface Methods {
-
-	public void removeFirst () ;	//to remove first card in initial shuffled deck
-	public void addFirst () ;		//to add the removed 1st card to each of the deck of the players
-}
